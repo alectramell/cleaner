d0042b60b1bc75f50451c51b5396ded08816831d
alectramell <alectramell@gmail.com>
2015-02-06 17:33:54 -0700

    CLEANER

---
+++ __main__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import os
+
+os.system('clear')
+
+commix = ('''
+gnome-terminal --title="THE CLEANER v1.0 | System Cleaning Utility" -x sh -c "sudo apt-get install deborphan && clear && sudo apt-get clean && sudo apt-get autoclean && sudo apt-get autoremove && sudo deborphan | xargs sudo apt-get -y remove --purge && clear && echo DONE! SQEEKY CLEAN! && sleep 5"
+''')
+
+commix = str(commix)
+
+os.system(commix)
+
+os.system('clear')
