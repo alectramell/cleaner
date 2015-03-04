277e4b9a789bbd561bca5e737f15f610215a3e70
alectramell <alectramell@gmail.com>
2015-03-03 16:58:33 -0800

    CLEANER

--- __main__.py
+++ __main__.py
@@ -5,7 +5,7 @@ import os
 os.system('clear')
 
 commix = ('''
-gnome-terminal --hide-menubar --full-screen --title="THE CLEANER v1.0 | System Cleaning Utility" -x sh -c "sudo apt-get install deborphan && clear && sudo apt-get clean && sudo apt-get autoclean && sudo apt-get autoremove && sudo apt-get check && sudo apt-get update && sudo deborphan | xargs sudo apt-get -y remove --purge && clear && echo FINISHED! && sleep 4"
+gnome-terminal --hide-menubar --full-screen --title="THE CLEANER v1.0 | System Cleaning Utility" -x sh -c "echo "THE CLEANER v1.0 | System Cleaning Utiltity" && sudo apt-get install deborphan && clear && sudo apt-get clean && sudo apt-get autoclean && sudo apt-get autoremove && sudo apt-get check && sudo apt-get update && sudo deborphan | xargs sudo apt-get -y remove --purge && clear && echo FINISHED! && read && clear"
 ''')
 
 commix = str(commix)
